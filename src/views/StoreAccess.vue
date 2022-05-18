2ff4d6fb847e6c240cef45fb5d960af7c9d4d187
leeyongju <dydwn5390@mqnic.com>
2022-05-18 17:53:13 +0900

    12장 완료

---
+++ src/views/StoreAccess.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>Count: {{count}}</p>
+  <button type="button" @click="increment">Increment</button>
+</template>
+
+<script>
+export default {
+  name: "StoreAccess",
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  computed: {
+    count(){
+      return this.$store.state.count;
+    }
+  },
+  setup() {
+  },
+  created() {
+  },
+  mounted() {
+  },
+  unmounted() {
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
