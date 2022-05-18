b77e7bf09969b472a8022d9bb40575d4113da33e
leeyongju <dydwn5390@mqnic.com>
2022-05-18 16:39:48 +0900

    8장 완료

---
+++ src/views/ChildComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <button type="button" @click="childFunc" ref="btn">click</button>
+  <h1>{{msg}}</h1>
+  <button type="button" @click="childFunc2" ref="btn2">자식 컴포넌트 데이터 변경</button>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      msg: '',
+      msg2: "자식 컴포넌트로부터 보내는 메세지",
+      msg3: "메세지"
+    }
+  },
+  setup() {
+  },
+  created() {
+  },
+  mounted() {
+    this.$emit('send-message', this.msg2);
+  },
+  unmounted() {
+  },
+  methods: {
+    childFunc() {
+      console.log("부모 컴포넌트에서 직접 발생시킨 이벤트");
+    },
+    childFunc2() {
+      this.msg3 = "변경된 메세지";
+    },
+    callFromParent() {
+      console.log("부모 컴포넌트에서 직접 호출한 함수");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/NestedComponent.vue
+++ src/views/NestedComponent.vue
@@ -5,16 +5,29 @@
   <div>
     <PageTitle v-bind:title="title"/>
   </div>
+  <div>
+    <child-component @send-message="sendMessage" ref="child_component"/>
+  </div>
+  <div>
+    <button type="button" @click="changeChildData">Change Child Data</button>
+  </div>
+  <hr>
+  <div>
+    <button type="button" @click="checkChild">자식 컴포넌트 데이터 조회</button>
+    <child-component ref="child_component"></child-component>
+  </div>
 </template>
 
 <script>
 import PageTitle from "@/components/PageTitle"; // 컴포넌트 import
+import ChildComponent from "@/views/ChildComponent";
+
 export default {
   name: "NestedComponent",
-  components: {PageTitle},  // 현재 페이지에서 사용할 컴포넌트 등록
+  components: {PageTitle, ChildComponent},  // 현재 페이지에서 사용할 컴포넌트 등록
   data() {
     return {
-      title: "동적 Prop 전달"
+      title: "동적 Prop 전달",
     }
   },
   setup() {
@@ -22,10 +35,27 @@ export default {
   created() {
   },
   mounted() {
+    //this.$refs.child_component.$refs.btn.click(); // 주석처리 안하면 mount 될때 바로 실행됨..
+    this.$refs.child_component.callFromParent();
   },
   unmounted() {
   },
-  methods: {}
+  computed: {
+    msg(){
+      return this.$refs.child_component.msg3;
+    }
+  },
+  methods: {
+    changeChildData() {
+      this.$refs.child_component.msg = "부모 컴포넌트가 변경한 데이터";
+    },
+    sendMessage(data) {
+      console.log(data);
+    },
+    checkChild() {
+      alert(this.msg);
+    }
+  }
 }
 </script>
 
